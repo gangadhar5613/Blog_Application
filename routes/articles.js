423d5a277a8931b9861a323fac38640cad90d2a7
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-12-10 22:49:15 +0530

    Make few changes in blog application

--- routes/articles.js
+++ routes/articles.js
@@ -41,6 +41,11 @@ router.get('/:id',(req,res,next) => {
   })
 })
 
+// router.get('/:id',(req,res,next) => {
+//   let id = req.params.id;
+//   Article.findById(id).populate("comments").exec()
+// })
+
 
 
 router.get("/:id/edit", (req, res, next) => {
